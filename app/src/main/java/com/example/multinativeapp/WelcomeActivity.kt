7d717033859c04b2bf58ebb072fe0efc6a512d82
Pablo Cortes <pcortes@cobiscorp.com>
2025-03-01 21:24:21 -0500

    Se añaden caracteristicas del taller N°3

--- app/src/main/java/com/example/multinativeapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/multinativeapp/WelcomeActivity.kt
@@ -1,11 +1,47 @@
 package com.example.multinativeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var buttonStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        Log.d("WelcomeActivity", "onCreate: Iniciando Welcome Activity")
+
+        buttonStart = findViewById(R.id.bt_get_in)
+        buttonStart.setOnClickListener{
+            //redireccionamiento
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("WelcomeActivity", "onStart: Welcome Activity está en primer plano")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("WelcomeActivity", "onPause: Welcome Activity está pausada")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("WelcomeActivity", "onStop: Welcome Activity está pausada")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("WelcomeActivity", "onDestroy: Welcome Activity está destruida")
     }
 }
